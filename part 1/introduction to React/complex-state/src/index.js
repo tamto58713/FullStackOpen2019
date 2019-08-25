09945df696bb506c1669c7f50b766d3a5bb43329
Tam To Tran <tamtotran5667@gmail.com>
2019-08-26 05:27:49 +0700

    complete exercise ... 1.10

--- part 1/introduction to React/complex-state/src/index.js
+++ part 1/introduction to React/complex-state/src/index.js
@@ -3,35 +3,73 @@ import ReactDOM from 'react-dom';
 
 const Good = (props) => {
     return (
-        <div>
-            Good: { props.good }
-        </div>
+        <tr>
+            <td>Good</td>
+            <td>{ props.good }</td>
+        </tr>
     )
 }
 
 const Neutral = (props) => {
     return (
-        <div>
-            Neutral: { props.neutral }
-        </div>
+        <tr>
+            <td>Neutral</td>
+            <td>{ props.neutral }</td>
+        </tr>
     )
 }
 
 const Bad = (props) => {
     return (
-        <div>
-            Good: { props.bad }
-        </div>
+        <tr>
+            <td>Bad</td>
+            <td>{ props.bad }</td>
+        </tr>
     )
 }
 
+const All = (props) => {
+    return (
+        <tr id="all">
+            <td>All</td>
+            <td>{ props.all }</td>
+        </tr>
+    )
+}
 
+const Average = (props) => {
+    return (
+        <tr>
+            <td id="average">
+                Average
+            </td>
+            <td>
+                { props.average }
+            </td>
+        </tr>
+    )
+}
+
+const Positive = (props) => {
+    return (
+        <tr>
+            <td id="positive">
+                Positive
+            </td>
+            <td>
+                { props.positive }%
+            </td>
+    </tr>
+    )
+}
 const App = (props) => {
 
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
-
+    const all = good + neutral + bad;
+    const average = (good - bad) / all || 0;
+    const positive = good / all * 100 || 0;
     const onClickFeedbackGood = () => {
         setGood(good + 1);
     }
@@ -45,17 +83,27 @@ const App = (props) => {
     return (
         <div>
             <h3>Give a feedback</h3>
-            <div id="buttons-feedback" class="btn-group">
+            <div id="buttons-feedback" className="btn-group">
                 <button className="btn btn-primary" onClick={onClickFeedbackGood} >Good</button>
                 <button className="btn btn-primary" onClick={onClickFeedbackNeutral}>Neutral</button>
                 <button className="btn btn-primary"  onClick={onClickFeedbackBad}>Bad</button>
             </div>
-            <h4>Statisfics</h4>
-            <div id="status">
-                <Good good={good}/>
-                <Neutral neutral={neutral}/>
-                <Bad bad={bad} />
-            </div>
+
+            <h3>Statisfics</h3>
+            {good === 0 && neutral === 0 && bad === 0 ? <h6>No feedback given</h6>:
+                                                                                    <table className="table" id="status">
+                                                                                        <thead></thead>
+                                                                                        <tbody>
+                                                                                        <Good good={good}/>
+                                                                                        <Neutral neutral={neutral}/>
+                                                                                        <Bad bad={bad} />
+                                                                                        <All all={all} />
+                                                                                        <Average average={average} />
+                                                                                        <Positive positive={positive} />
+                                                                                        </tbody>
+                                                                                    </table >}
+                                                            
+
         </div>
     )
 }
